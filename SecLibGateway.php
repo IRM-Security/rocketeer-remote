b41bce756f7364d678bdef38a053113ad6bcb44d
krisztian.mariassy <krisztian.mariassy@irmsecurity.com>
2024-08-14 16:18:29 +0100

    Updated more occurances

--- SecLibGateway.php
+++ SecLibGateway.php
@@ -141,7 +141,7 @@ class SecLibGateway implements GatewayInterface {
 	 */
 	public function put($local, $remote)
 	{
-		$this->getConnection()->put($remote, $local, NET_SFTP_LOCAL_FILE);
+		$this->getConnection()->put($remote, $local, phpseclib\Net\SFTP::SOURCE_LOCAL_FILE);
 	}
 
 	/**
@@ -163,7 +163,7 @@ class SecLibGateway implements GatewayInterface {
 	 */
 	public function nextLine()
 	{
-		$value = $this->getConnection()->_get_channel_packet(NET_SSH2_CHANNEL_EXEC);
+        $value = $this->getConnection()->_get_channel_packet(phpseclib\Net\SSH2::CHANNEL_EXEC);
 
 		return $value === true ? null : $value;
 	}
